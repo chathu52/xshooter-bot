55e05044469fd2a6175adcecfaa89a36c981e7cf
chathu52 <tcschathu@gmail.com>
2025-06-28 07:41:26 +0530

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-# config.py
-
-# Replace with your actual keys
-TELEGRAM_BOT_TOKEN = "8182078711:AAFUssRrseut4az4htJ_72aQbWvAJ1Ngdfs"
-TELEGRAM_CHAT_ID = "5969163683"
-
-CLAUDE_API_KEY = "sk-ant-api03-..."  # Your real key (this stays local, not committed to GitHub)
-
